0b77f98e258ea178d41f45ebe784935c5061e970
Russell Barthel <russell.barthel@gmail.com>
2017-10-12 20:48:28 +0000

    add register and login capability

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  # before_filter :authorize # This will force user's to login before they can see the actions in this controller
+
   def show
   end
 end
