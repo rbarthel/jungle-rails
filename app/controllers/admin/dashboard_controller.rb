b1774ab371124adeb95315f48141a61a60986cf6
Russell Barthel <russell.barthel@gmail.com>
2017-10-13 00:40:08 +0000

    add admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
 
-  # before_filter :authorize # This will force user's to login before they can see the actions in this controller
+  before_filter :authenticate
 
   def show
   end
