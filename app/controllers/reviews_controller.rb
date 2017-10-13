c12d542c882b7af9d13e19079bab221597bb846c
Russell Barthel <russell.barthel@gmail.com>
2017-10-13 22:40:47 +0000

    users can add reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,15 @@
+class ReviewsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    @review.save
+    redirect_to @product
+  end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:rating, :description, :user_id, :product_id)
+    end
+end
