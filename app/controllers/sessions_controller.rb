aeba32aea02b4f96e82adf03aebbc9c36be2bf23
Russell Barthel <russell.barthel@gmail.com>
2017-10-14 03:06:18 +0000

    style with css and bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
+      flash[:danger] = 'Username or password incorrect.'
       render :new
     end
   end
